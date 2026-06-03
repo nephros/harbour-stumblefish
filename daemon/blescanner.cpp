ff566bfff752c023f0bbde652c486468f2af92b1
nephros <nemo@pgxperiiia10>
2026-06-03 22:41:35 +0200

    Glassfish: add subsubsubversion

--- daemon/blescanner.cpp
+++ daemon/blescanner.cpp
@@ -62,7 +62,7 @@ BleScanner::BleScanner(QObject *parent)
                                          QDBusConnection::systemBus(),
                                          this))
     , m_enabled(false)
-    , m_alertsenabled(APP_VERSION == NULL)
+    , m_alertsenabled(QString(APP_VERSION).isEmpty())
     , m_status(QStringLiteral("disabled"))
 {
     qDBusRegisterMetaType<InterfaceList>();
