e00a7b92598e7e9304efe22fc1afcb2291311087
nephros <nemo@JollaC2>
2026-06-16 20:54:06 +0200

    Add args to call

--- glassfish/src/glassfish.cpp
+++ glassfish/src/glassfish.cpp
@@ -17,6 +17,15 @@ bool Glassfish::bleCollectionEnabled()
 								Stumblefish::InterfaceName,
 								QStringLiteral("report")
 	);
+	QList<QVariant> args;
+	args << QVariant(0);
+	message.setArguments(args);
 	reply = QDBusConnection::sessionBus().call(message);
+	/*
+	 * busctl --user call org.stumblefish.Collector /org/stumblefish/Collector  org.stumblefish.Collector report i 0
+	 * a{sv} 21 "accuracy" d -1 "altitude" d 0 "ble" av 0 "bleCount" i 0 "bleEnabled" b false "cellCount" i 0 "cellEnabled" b fals
+	 * e "cells" av 0 "endpoint" s "" "id" i 0 "lastError" s "" "latitude" d 0 "longitude" d 0 "mode" s "" "retryCount" i 0 "times
+	 * tampMs" x 0 "uploadStatus" s "" "uploadedAtMs" x 0 "wifi" av 0 "wifiCount" i 0 "wifiEnabled" b false
+	 */
     return false;
 }
