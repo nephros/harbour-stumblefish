9ebb62ff9ccd1a0d68b30bf29c58d221910cf3a1
nephros <nemo@JollaC2>
2026-06-17 19:58:03 +0200

    Align main.cpp somewhat with the others

--- glassfish/main.cpp
+++ glassfish/main.cpp
@@ -1,12 +1,26 @@
 #include <QCoreApplication>
 
+#include "../common/constants.h"
 #include "glassfish.h"
+
+const char OrganizationName[] = "org.stumblefish";
+const char ApplicationName[] = "harbour-glassfish";
 
 int main(int argc, char* argv[])
 {
     QCoreApplication app(argc, argv);
+    app.setOrganizationName(QString::fromLatin1(OrganizationName));
+    app.setOrganizationDomain(QStringLiteral("stumblefish.org"));
+    app.setApplicationName(QString::fromLatin1(ApplicationName));
+    app.setApplicationVersion(QStringLiteral(APP_VERSION));
+
     Glassfish* fish = new Glassfish();
-    if (!fish->enabled()) return 1;
+    //if (!fish->enabled()) {
+    if (0) {
+        qDebug() << "Glassfish (or BLE scanning) is not enabled";
+        return 1;
+    }
+    qInfo() << qPrintable(app.applicationName()) << "started";
 
     QList<QVariantMap> reports = fish->getReports();
 
