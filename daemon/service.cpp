b0e4c20cbf46d62861258514bfa9e687102f8e46
Peter G. (nephros) <nemo@PGJolla26-I>
2026-09-12 19:11:17 +0200

    Track Daemon: move additional observations from service to phonetrack

--- daemon/service.cpp
+++ daemon/service.cpp
@@ -213,10 +213,6 @@ QVariantMap Service::status() const
     map.insert(QStringLiteral("longitude"), fix.longitude);
     map.insert(QStringLiteral("accuracy"), fix.accuracy);
     map.insert(QStringLiteral("fixTimestampMs"), fix.timestampMs);
-#ifdef TRACK_MY_PHONE
-    map.insert(QStringLiteral("direction"), fix.direction);
-    map.insert(QStringLiteral("speed"), fix.speed);
-#endif
     return map;
 }
 
