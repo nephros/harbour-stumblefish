9ec0ea3beb1639924091af928beb9042319fc7fc
Peter G. (nephros) <nemo@PGJolla26-I>
2026-09-12 18:15:42 +0200

    Companions: create/update header files

--- common/constants.h
+++ common/constants.h
@@ -12,24 +12,10 @@ const char InterfaceName[] = "org.stumblefish.Collector";
 const char DefaultEndpoint[] = "https://api.beacondb.net/v2/geosubmit";
 const char OrganizationName[] = "org.stumblefish";
 const char ApplicationName[] = "harbour-stumblefish";
-
 }
 
 #ifdef TRACK_MY_PHONE
 #include "phonetrackconfig.h"
-namespace Trackfish {
-
-const char ServiceName[] = "org.stumblefish.Tracker";
-const char ObjectPath[] = "/org/stumblefish/Tracker";
-const char InterfaceName[] = "org.stumblefish.Tracker";
-const char OrganizationName[] = "org.stumblefish";
-const char ApplicationName[] = "harbour-trackfish";
-const QByteArray UserAgent = QStringLiteral("harbour-stumblefish/%1 (%2)")
-                             .arg(QStringLiteral(APP_VERSION))
-                             .arg(QStringLiteral("PhoneTrack Companion"))
-                             .toUtf8();
-}
-
 #endif
 
 #endif
--- companions/common/common.pri
+++ companions/common/common.pri
@@ -1,3 +1,11 @@
-INCLUDEPATHS += $PWD
+GLOBAL_COMMON_DIR = ../../common
+COMPANIONS_COMMON_DIR = $PWD
+
+INCLUDEPATHS += \
+            $${COMPANIONS_COMMON_DIR} \
+            $${GLOBAL_COMMON_DIR}
+
 HEADERS += \
+    versions.h \
+    constants.h \
     abstractcompanion.h
