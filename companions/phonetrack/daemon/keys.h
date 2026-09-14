530927417c49885fdb6f2fc4a2d21dac72cacba2
Peter G. (nephros) <nemo@PGJolla26-I>
2026-09-15 00:12:45 +0200

    Track Daemon: add missing file

---
+++ companions/phonetrack/daemon/keys.h
@@ -0,0 +1,9 @@
+namespace {
+const char PhoneTrackEnableKey[] = "phonetrack/enable";
+const char PhoneTrackLiveKey[] = "phonetrack/liveMode";
+const char PhoneTrackTypeKey[] = "phonetrack/type";
+const char PhoneTrackUrlKey[] = "phonetrack/url";
+const char PhoneTrackSessionKey[] = "phonetrack/session";
+const char PhoneTrackNameKey[] = "phonetrack/name";
+}
+
