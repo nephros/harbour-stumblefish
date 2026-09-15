37fab9162139e29fe949ca5cb84647b9d484b849
Peter G. (nephros) <nemo@PGJolla26-I>
2026-09-15 15:49:32 +0200

    Companions: update plugin a bit

--- companions/client/plugin/plugin.cpp
+++ companions/client/plugin/plugin.cpp
@@ -1,13 +1,18 @@
+#include <QObject>
 #include <QtQml>
 
-class TrackFishPlugin : public QQmlExtensionPlugin
+//void qml_register_types_my_module();
+//volatile auto registration = &qml_register_types_my_module;
+//Q_UNUSED(registration);
+
+class CompanionPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.stumblefish.trackfish")
+    Q_PLUGIN_METADATA(IID "org.stumblefish.companions")
 public:
     void registerTypes(const char *uri)
     {
-        Q_ASSERT(uri == QLatin1String("org.stumblefish.trackfish"));
-        qmlRegisterType<TrackFishPlugin>(uri, 0, 1, "PhoneTrack");
+        Q_ASSERT(uri == QLatin1String("org.stumblefish.companions"));
+        qmlRegisterType<CompanionPlugin>(uri, 1, 0, "Companion");
     }
 };
--- companions/client/plugin/plugin.pro
+++ companions/client/plugin/plugin.pro
@@ -1,12 +1,20 @@
 TEMPLATE = lib
-TARGET = trackfishqml
+TARGET = stumblecompanions
+TARGET = $$qtLibraryTarget($$TARGET)
+CONFIG += qt plugin qmltypes
+
+QT += qml
+QT -= gui dbus network
+
+QML_IMPORT_NAME = org.stumblefish.companions
+QML_IMPORT_MAJOR_VERSION = 1
+QML_IMPORT_PATH = /usr/share/harbour-stumblefish/lib
 
 message("Building the client QML plugin")
 
-CONFIG += qt plugin
-QT += qml quick
-
 SOURCES += plugin.cpp
 
-INSTALLS += target
+qmldir.files += qmldir
+qmldir.path = /usr/share/harbour-stumblefish/lib
 target.path = /usr/share/harbour-stumblefish/lib
+INSTALLS += qmldir target
